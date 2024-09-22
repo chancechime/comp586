d52194c48bd92963cb47e1c63b3ce87fb8630b73
Chance Chime <cchime82@icloud.com>
2024-09-21 21:07:45 -0700

    Update .gitignore to exclude specific files and directories

--- Project 1/obj/Debug/net8.0/Project1.AssemblyInfo.cs
+++ Project 1/obj/Debug/net8.0/Project1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40c5da2a7d108b2edb79c9130c9fe6b29115081f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff64fe4574a3e4cd7df16b813b3b75e0a906bfcc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
