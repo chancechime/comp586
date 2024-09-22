344270aa63de38ce2d3bf737eac2cdcbe30eaec0
Chance Chime <cchime82@icloud.com>
2024-09-21 21:04:01 -0700

    Update assembly version and source link URLs

--- Project 1/obj/Debug/net8.0/Project1.AssemblyInfo.cs
+++ Project 1/obj/Debug/net8.0/Project1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62a05dd0c1b00b045c83d2c9a304c810d5e62de3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da86281ab2a80e0231e0ad5ecae8ad00930057d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
